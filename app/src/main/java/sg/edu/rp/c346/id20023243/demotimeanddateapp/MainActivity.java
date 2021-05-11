81e83577ed799ded6642083826153b640fea01a8
ariqah <20023243@rp.edu.sg>
2021-05-11 20:57:23 +0800

    Commit demotime&date

---
+++ app/src/main/java/sg/edu/rp/c346/id20023243/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,56 @@
+package sg.edu.rp.c346.id20023243.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate, btnDisplayTime, btnReset;
+    TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.buttonReset);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String timeIs = "Time is "+tp.getCurrentHour()+":"+ String.format("%02d",tp.getCurrentMinute());
+                tvDisplay.setText(timeIs);
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dateIs = "Date is "+dp.getDayOfMonth()+"/"+(dp.getMonth()+1)+"/"+dp.getYear();
+                tvDisplay.setText(dateIs);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dp.updateDate(2021,0,1);
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+            }
+        });
+
+    }
+}
